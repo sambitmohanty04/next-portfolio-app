90ecb1101a24f2fbae85b210f0aad351bc939f39
Sam <sammohanty04@gmail.com>
2026-03-17 17:58:56 +0530

    fix: added types for params in TeamDetailsPage

--- app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AboutPage = () => {
-  return (
-    <div className='text-white'>AboutPage</div>
-  )
-}
-
-export default AboutPage
--- app/about/team/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TeamPage = () => {
-  return (
-    <div className='text-white'>TeamPage</div>
-  )
-}
-
-export default TeamPage
